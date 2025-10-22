b9dacbb4279d22e9a7cb573624b71b7ba9a67399
jgkiran2003 <jgkiran2003@gmail.com>
2025-10-22 23:38:27 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,10 +1,13 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-import static java.util.Objects.requireNonNull;
-
+/**
+ * Redoes the last address book modifying operation that was undone.
+ */
 public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-
 
 public class RedoCommandTest {
     private Model model = new ModelManager();
